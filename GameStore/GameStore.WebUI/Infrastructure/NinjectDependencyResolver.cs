833734f1cb1dc0da527e9e201a700c5865a7105e
Orion10110 <pojasoriona@gmail.com>
2016-08-29 16:43:57 +0300

    Revert "Include file db (gamestore.db)"
    This reverts commit a5d7b076d8ab6dd793c4d474b519959475c86534.

--- GameStore/GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ GameStore/GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -6,7 +6,6 @@ using Moq;
 using Ninject;
 using GameStore.Domain.Abstract;
 using GameStore.Domain.Entities;
-using GameStore.Domain.Concrete;
 
 namespace GameStore.WebUI.Infrastructure
 {
@@ -32,7 +31,14 @@ namespace GameStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            kernel.Bind<IGameRepository>().To<EFGameRepository>();
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+    {
+        new Game { Name = "SimCity", Price = 1499 },
+        new Game { Name = "TITANFALL", Price=2299 },
+        new Game { Name = "Battlefield 4", Price=899.4M }
+    });
+            kernel.Bind<IGameRepository>().ToConstant(mock.Object);
         }
     }
 }
