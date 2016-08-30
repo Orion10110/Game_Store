de748f2b3bf0fdffe0899ee083720c1444698bcb
Orion10110 <pojasoriona@gmail.com>
2016-08-30 17:56:18 +0300

    added edit admin page

--- GameStore/GameStore.WebUI/Controllers/AdminController.cs
+++ GameStore/GameStore.WebUI/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,29 @@ namespace GameStore.WebUI.Controllers
         public ViewResult Index()
         {
             return View(repository.Games);
+        }
+
+        public ViewResult Edit(int gameId)
+        {
+            Game game = repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+            return View(game);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveGame(game);
+                TempData["message"] = string.Format("Изменения в игре \"{0}\" были сохранены", game.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // Что-то не так со значениями данных
+                return View(game);
+            }
         }
     }
 }
