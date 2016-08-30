a45b37d2ab6c9223d8d569dcde8c1e354a5e888c
Orion10110 <pojasoriona@gmail.com>
2016-08-30 13:00:53 +0300

    added navigation

--- GameStore/GameStore.WebUI/App_Start/RouteConfig.cs
+++ GameStore/GameStore.WebUI/App_Start/RouteConfig.cs
@@ -9,21 +9,40 @@ namespace GameStore.WebUI
 {
     public class RouteConfig
     {
-        public static void RegisterRoutes(RouteCollection routes)//a
+        public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(null,
+                "",
+                new
+                {
+                    controller = "Game",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                }
+            );
 
             routes.MapRoute(
                 name: null,
                 url: "Page{page}",
-                defaults: new { controller = "Game", action = "List" }
+                defaults: new { controller = "Game", action = "List", category = (string)null },
+                constraints: new { page = @"\d+" }
             );
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Game", action = "List", id = UrlParameter.Optional }
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Game", action = "List", page = 1 }
             );
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Game", action = "List" },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
--- GameStore/GameStore.WebUI/Controllers/GameController.cs
+++ GameStore/GameStore.WebUI/Controllers/GameController.cs
@@ -17,20 +17,24 @@ namespace GameStore.WebUI.Controllers
         {
             repository = repo;
         }
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category,int page = 1)
         {
             GamesListViewModel model = new GamesListViewModel
             {
                 Games = repository.Games
-                     .OrderBy(game => game.GameId)
-                     .Skip((page - 1) * pageSize)
-                     .Take(pageSize),
+                    .Where(p => category == null || p.Category == category)
+                    .OrderBy(game => game.GameId)
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize),
                 PagingInfo = new PagingInfo
                 {
                     CurrentPage = page,
                     ItemsPerPage = pageSize,
-                    TotalItems = repository.Games.Count()
-                }
+                    TotalItems = category == null ?
+        repository.Games.Count() :
+        repository.Games.Where(game => game.Category == category).Count()
+                },
+                CurrentCategory = category
             };
             return View(model);
         }
